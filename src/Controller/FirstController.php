75c0a7c359eafce56f7ec0bc316dd9aba23b6b7b
bhaveshpadave <bhaveshpadave97@gmail.com>
2024-06-10 16:08:14 +0530

    Add twig changes

--- src/Controller/FirstController.php
+++ src/Controller/FirstController.php
@@ -2,22 +2,20 @@
 // src/Controller/FirstController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class FirstController
+class FirstController extends AbstractController
 {
     #[Route('/firstcontroller')]
-    public function number(): Response {
+    public function controller(): Response {
+
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html>
-                <body>
-                    <h1>First Controller</h1>
-                    First number: '.$number.'
-                </body>
-            </html>'
-        );
+        return $this->render('first/first.html.twig', [
+            'rand_number' => $number,
+        ]);
+
     }
 }
